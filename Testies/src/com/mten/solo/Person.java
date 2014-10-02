5d6192630b9e426b22b926d1d71360b72bd36af4
MTen <MichaelTener@gmail.com>
2014-10-02 14:50:50 -0400

    Made Testies project
    Reinforce learning by doing the steps without the tutorial or any
    assistance.

---
+++ Testies/src/com/mten/solo/Person.java
@@ -0,0 +1,16 @@
+package com.mten.solo;
+
+public class Person {
+	//fields
+	private String name;
+	private int maxBooksAllowed;
+	
+	//constructors
+	
+	public Person() {
+		name = "unknown name";
+		maxBooksAllowed = 3;
+	}
+
+}
+
