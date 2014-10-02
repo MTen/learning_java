363391e12388be6563e41e973eee32289a17f725
MTen <MichaelTener@gmail.com>
2014-10-02 15:31:12 -0400

    wrote it myself in testies
    rewrote getter and setter methods manually, with eclipse assist and with
    eclipse source writing the the methods for me.

--- Testies/src/com/mten/solo/Person.java
+++ Testies/src/com/mten/solo/Person.java
@@ -11,6 +11,30 @@ public class Person {
 		name = "unknown name";
 		maxBooksAllowed = 3;
 	}
+	
+	//methods
+	
+	//getter for name
+	public String getName() {
+		return name;
+	}
+	//setter for name
+	public void setName(String anyName) {
+		name = anyName;
+	}
+	
+	/**
+	 * @return the maxBooksAllowed
+	 */
+	public int getMaxBooksAllowed() {
+		return maxBooksAllowed;
+	}
 
+	/**
+	 * @param maxBooksAllowed the maxBooksAllowed to set
+	 */
+	public void setMaxBooksAllowed(int maxBooksAllowed) {
+		this.maxBooksAllowed = maxBooksAllowed;
+	}	
 }
 
