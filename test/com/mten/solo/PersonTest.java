ca641e1472bc6ed8a45d9543833f2b6e30f3a50f
Mten <MichaelTener@gmail.com>
2014-10-03 12:39:48 -0400

    Wrote my own tests and verified that local variables are contained within the scope of each individual test.

--- test/com/mten/solo/PersonTest.java
+++ test/com/mten/solo/PersonTest.java
@@ -5,23 +5,21 @@ import junit.framework.TestCase;
 public class PersonTest extends TestCase {
 
 	public void testPerson() {
-		fail("Not yet implemented");
-	}
-
-	public void testGetName() {
-		fail("Not yet implemented");
+		Person p1 = new Person();
+		assertEquals("unknown name", p1.getName());
+		assertEquals(3, p1.getMaxBooksAllowed());
 	}
 
 	public void testSetName() {
-		fail("Not yet implemented");
-	}
-
-	public void testGetMaxBooksAllowed() {
-		fail("Not yet implemented");
+		Person p1 = new Person();
+		p1.setName("Billy");
+		assertEquals("Billy", p1.getName());
 	}
 
 	public void testSetMaxBooksAllowed() {
-		fail("Not yet implemented");
+		Person p1 = new Person();
+		p1.setMaxBooksAllowed(10);
+		assertEquals(10, p1.getMaxBooksAllowed());
 	}
 
 }
