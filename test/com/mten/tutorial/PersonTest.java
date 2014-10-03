aa1c6b514d4cb187bf5185f20cc97e192922a632
Mten <MichaelTener@gmail.com>
2014-10-03 12:22:37 -0400

    Tested some methods after the methods were already written for getter and setters.

--- test/com/mten/tutorial/PersonTest.java
+++ test/com/mten/tutorial/PersonTest.java
@@ -5,15 +5,21 @@ import junit.framework.TestCase;
 public class PersonTest extends TestCase {
 
 	public void testPerson() {
-		fail("Not yet implemented");
+		Person p1 = new Person();
+		assertEquals("unknown name", p1.getName());
+		assertEquals(3, p1.getMaximumBooks());
 	}
 
 	public void testSetName() {
-		fail("Not yet implemented");
+		Person p2 = new Person();
+		p2.setName("Fred");
+		assertEquals("Fred", p2.getName());
 	}
 
 	public void testSetMaximumBooks() {
-		fail("Not yet implemented");
+		Person p3 = new Person();
+		p3.setMaximumBooks(10);
+		assertEquals(10, p3.getMaximumBooks());
 	}
 
 }
