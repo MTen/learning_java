a04ab151fc199f49d4c1df6e44c555aa8f7f6bb0
Mten <MichaelTener@gmail.com>
2014-10-03 11:59:23 -0400

    Learned how to build a test suite with eclipse and then rebuilt a test suite on my own.

---
+++ test/com/mten/solo/PersonTest.java
@@ -0,0 +1,27 @@
+package com.mten.solo;
+
+import junit.framework.TestCase;
+
+public class PersonTest extends TestCase {
+
+	public void testPerson() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetMaxBooksAllowed() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetMaxBooksAllowed() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/com/mten/tutorial/PersonTest.java
@@ -0,0 +1,19 @@
+package com.mten.tutorial;
+
+import junit.framework.TestCase;
+
+public class PersonTest extends TestCase {
+
+	public void testPerson() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetMaximumBooks() {
+		fail("Not yet implemented");
+	}
+
+}
