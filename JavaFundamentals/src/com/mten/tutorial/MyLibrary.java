1264a31dbdd8f3ab7c44a2e3fe773b4206e7b90f
Mten <MichaelTener@gmail.com>
2014-10-03 21:45:11 -0400

    Added main method and mock program behavior.

--- JavaFundamentals/src/com/mten/tutorial/MyLibrary.java
+++ JavaFundamentals/src/com/mten/tutorial/MyLibrary.java
@@ -105,4 +105,48 @@ public class MyLibrary {
 				this.getBooks().size() + " books; " +
 				this.getPeople().size() + " people.";
 	}
+	
+	public static void main(String[] args) {
+		// create a new MyLibrary
+		MyLibrary testLibrary = new MyLibrary("Test Driven Library");
+		Book b1 = new Book("War and Peace");
+		Book b2 = new Book("Great Expectations");
+		b1.setAuthor("Tolstoy");
+		b2.setAuthor("Dickens");
+		Person jim = new Person();
+		Person sue = new Person();
+		jim.setName("Jim");
+		sue.setName("Sue");
+		
+		testLibrary.addBook(b1);
+		testLibrary.addBook(b2);
+		testLibrary.addPerson(jim);
+		testLibrary.addPerson(sue);
+		
+		System.out.println("Just created a new library");
+		testLibrary.printStatus();
+		System.out.println("Check out War and Peace to Sue");
+		testLibrary.checkOut(b1, sue);
+		testLibrary.printStatus();
+		
+		System.out.println("Do some more stuff");
+		testLibrary.checkIn(b1);
+		testLibrary.checkOut(b2, jim);
+		testLibrary.printStatus();
+		
+	}
+
+	private void printStatus() {
+		System.out.println("Status Report of MyLibrary \n" +
+				this.toString());
+		for (Book thisBook : this.getBooks()) {
+			System.out.println(thisBook);
+		}
+		for (Person p : this.getPeople()) {
+			int count = this.getBooksForPerson(p).size();
+			System.out.println(p + " (has " + count + " of my books)");
+		}
+		System.out.println("Books Available: " + this.getAvailableBooks().size());
+		System.out.println("--- End of Status Report ---");
+	}
 }
